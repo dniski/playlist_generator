a73fd84ec40a06475b857d0c6c78bdf39fc444a3
deni <102609618+dniski@users.noreply.github.com>
2024-01-12 00:24:16 +0200

    feat(client): create react component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import styles from './index.module.css'
+import spotifyLogo from './assets/social.png'
 function App() {
   const [count, setCount] = useState(0)
+  const [playlistName, setPlaylistName] = useState("");
+  const [playlistDescription, setPlaylistDescription] = useState("");
+  const [playlistLength, setPlaylistLength] = useState(0); 
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted!", playlistName + " \n " + playlistDescription + " \n" + playlistLength);
+  }
+
+  return(
+    <main className={styles.main}>
+      <img  src={spotifyLogo} alt="" className={styles.icon}/>
+      <h3>Generate music playlist with AI: </h3>
+
+      <form action="" onSubmit={onSubmit}>
+
+        <input type="text" 
+          name='playlist-name' 
+          placeholder="Name your playlist: "
+          onChange={(e) => setPlaylistName(e.target.value)}/>
+
+        <input type="text" 
+          name="playlist-description" 
+          placeholder="Describe your playlist:"
+          onChange={(e) =>  setPlaylistDescription(e.target.value)}/>
+
+        <input type="number" 
+          name="playlist-length" 
+          min="5" 
+          max="30" 
+          placeholder="Playlist length (5-30)"
+          onChange={(e) => setPlaylistLength(e.target.value)}/>
+
+        <input type="submit" value="Generate!"/>
+
+      </form>
+    </main>
   )
 }
 
